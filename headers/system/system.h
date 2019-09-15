aafc0b5adbab3c6c44f8020bf48196b30fba253b
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2019-09-15 14:48:02 +0200

    till 3.2. done

--- headers/system/system.h
+++ headers/system/system.h
@@ -99,24 +99,34 @@ using namespace std;
 #ifndef DEFINE_INIT
 #define DEFINE_INIT
 
+//   The width of the "screen" part of the console window.
 #define SCREENCOLS 39
 
+//   The width of the "screen" part of the console window.
 #define MENUCOLS 41
 
+//   The dimensions of the console window.
 #define CONSOLEROWS 24
 #define CONSOLECOLS 80
 
+//   The dimensions of the FOVs and FOV arrays.
 #define FOVROWS 21
 #define FOVCOLS 35
 
+//   The dimensions of the world map.
 #define WORLDROWS 63
 #define WORLDCOLS 231
 
+//   The amount of characters in the solidcount.txt file.
 #define SOLIDCOUNT 1
+//   The amount of characters in the walkablecount.txt file.
 #define WALKABLECOUNT 1
 
+//   A small amount that helps correcting cases like x = 1/3 = 0.333333
+// x * 3 = 0.999999 =/= 1. 
 #define INFINITECIMAL 0.000001
 
+//   The maximum amount of frames that can be in an animation.
 #define MAXAMOUNTOFANIMATIONFRAMES 500
 
 #endif //DEFINE_INIT
@@ -124,56 +134,84 @@ using namespace std;
 #ifndef STRUCT_INIT
 #define STRUCT_INIT
 
+//   This strcture holds entities in the world (right now these entities are just the
+// player and the camera).
 struct mob
 {
+	//   These variables hold the coordinate/the position of the mob/entity in the world.
 	int row;
 	int col;
+	//   These variables hold the orientation of the mob. There are 8 different
+	//  orientatins. Currentaly only the player entity uses these sub variables.
 	bool up;
 	bool down;
 	bool right;
 	bool left;
 };
 
+//   This structure holds information about the map in the engine.
 struct map
 {
+	//   This sub variable holds the texture of the cell.
 	char texture;
+	//   These sub variables hold the properties of the different cells. Solid si true 
+	// when the given cell blocks light, and walkable is true when the given cell does
+	// not block movement.
 	bool solid;
 	bool walkable;
+	//   These sub variables hold information about the visibility of the different 
+	// cells. When mapInView is true the cell is in view, when mapIsEdge is true a 
+	// less intense shadow gets displayed in the game on that cell.
 	bool mapInView;
 	bool mapIsEdge;
 };
 
+//   This structure holds information about the FOV arrays.
 struct fov
 {
+	//   This is true if the cell is in view in the FOV array.
 	bool inView;
+	//   This is true on the cell where the player is in the FOV array.
 	bool isPlayer;
 };
 
+//   This structure holds the variables that define a line in the engine.
 struct line
 {
+	//   This is the slope of the line.
 	double mSlope;
+	//   This is the point where the line intercepts the y axis.
 	double bIntercept;
+	//   This is true if the line is below any given object we want to "compare" it to.
 	bool isItUnderLine;
 };
 
+//   This structure holds two lines at once.
 struct edgeLines
 {
 	line first;
 	line second;
 };
 
+//   This structure holds coordinates.
 struct koordinate
 {
 	double x;
 	double y;
 };
 
+//   This structure holds animations.
 struct animation
 {
+	//   The number of frames in the animation.
 	int frames;
+	//   The current frame of the animation the engine is playing/displaying.
 	int currentFrame;
+	//   The "height" of every frame of the animation.
 	int height;
+	//   The "width" of every frame of the animation.
 	int width;
+	//   This array holds every frame of the animation.
 	char frameArray[MAXAMOUNTOFANIMATIONFRAMES][CONSOLEROWS][SCREENCOLS + MENUCOLS];
 };
 
